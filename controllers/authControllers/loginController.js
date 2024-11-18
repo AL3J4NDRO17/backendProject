115587ff3ec2c6f88bd85d67ed2ded3cc85325f8
AlejandroEspinosa <116526021+AL3J4NDRO17@users.noreply.github.com>
2024-11-17 22:15:19 -0600

    Update loginController.js

--- controllers/authControllers/loginController.js
+++ controllers/authControllers/loginController.js
@@ -128,7 +128,6 @@ exports.verifyOtp = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             maxAge: 24 * 60 * 60 * 1000, // 1 día en milisegundos
-            sameSite: 'none',
             path: '/',
         });
 
